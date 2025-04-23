2b9740673358d98c411c6c3f312766c0c42f8181
hashtag-kps <singhkavyanshpratap@gmail.com>
2025-04-23 15:23:22 +0530

    Initial Commit

---
+++ Solace/SolaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolaceApp.swift
+//  Solace
+//
+//  Created by Kavyansh Pratap Singh on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SolaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
